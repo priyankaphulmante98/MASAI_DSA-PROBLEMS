35c017cc554823b345722b6dbb75714de17ede83
priyanka <priyankaphulmante98@gmail.com>
2022-10-07 00:09:07 +0530

    3

--- MATRIX/rotate_By_90.js
+++ MATRIX/rotate_By_90.js
@@ -1,12 +1,12 @@
-// function rotateBy90(R, matrix){
-//     for(let col=R-1; col>=0; col--){
-//         let bag="";
-//         for(let row=0; row<R; row++){
-//             bag+=matrix[row][col]+" ";
-//         }
-//         console.log(bag);
-//     }
-// }
+function rotateBy90(R, matrix){
+    // for(let col=R-1; col>=0; col--){
+    //     let bag="";
+    //     for(let row=0; row<R; row++){
+    //         bag+=matrix[row][col]+" ";
+    //     }
+    //     console.log(bag);
+    // }
+}
 
 function rotateBy90(R, matrix){
     for(let col=R-1; col>=0; col--){
