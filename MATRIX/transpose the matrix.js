6c964ee19eb43ea09024ab3acf70d674d7c2924b
priyanka <priyankaphulmante98@gmail.com>
2022-10-06 23:47:40 +0530

    day-11

--- MATRIX/transpose the matrix.js
+++ MATRIX/transpose the matrix.js
@@ -1,10 +1,23 @@
+// function transposeTheMatrix(N, M, matrix){
+//     for(let col=0; col<M; col++){
+//         let bag="";
+//         for(let row=0; row<N; row++){
+//             bag+=matrix[row][col]+" ";
+//             }
+//             console.log(bag);
+//         }
+//     }
+  
+
 function transposeTheMatrix(N, M, matrix){
+
     for(let col=0; col<M; col++){
-        let bag="";
+            let bag="";
         for(let row=0; row<N; row++){
-            bag+=matrix[row][col]+" ";
-            }
-            console.log(bag);
+            bag=bag+matrix[row][col]+" ";
         }
+ console.log(bag);
+    }
+           
     }
   
